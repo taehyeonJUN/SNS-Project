4fc317ce7fc80543978a6ca8c9e48eec91ec7b0e
taehyeonJUN <seweiov@gmail.com>
2020-09-15 19:40:25 +0900

    자바 스프링 프로젝트 작성

--- spring/Tongrami/src/main/java/kr/co/tongrami/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package kr.co.tongrami.dao;
-
-import kr.co.tongrami.model.Member;
-
-public interface MemberDao {
-
-	void login(Member member);
-	
-}
--- spring/Tongrami/src/main/java/kr/co/tongrami/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.tongrami.service;
-
-import kr.co.tongrami.model.Member;
-
-public interface MemberService {
-
-	void login(Member member);
-
-}
