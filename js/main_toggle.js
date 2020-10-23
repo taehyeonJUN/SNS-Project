6c6c00f5fe5b547094fec3c10f212b05c01db1bc
yeezeh <s_cyril@naver.com>
2020-10-23 15:23:32 +0900

    10월 23일 // js, image 파일 //

--- js/main_toggle.js
+++ js/main_toggle.js
@@ -1,10 +1,12 @@
-const toggleBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.nav-btn');
+// const toggleBtn = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.nav-btn');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
+// toggleBtn.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+// });
 
+
+// 본문 쩜쩜쩜 토글
 const toggleBtn = document.querySelector('.setting-btn');
 const menu = document.querySelector('.delete-btn');
 
